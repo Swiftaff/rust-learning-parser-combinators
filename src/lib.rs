c8fa5bcd34f1026e90d280e6f79d554aedc0adb8
swiftaff <swiftaff@gmail.com>
2020-04-27 17:10:58 +0800

    a parser builder

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,10 @@ fn the_letter_a(input: &str) -> Result<(&str, ()), &str> {
         _ => Err(input),
     }
 }
+
+fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), &str> {
+    move |input| match input.get(0..expected.len()) {
+        Some(next) if next == expected => Ok((&input[expected.len()..], ())),
+        _ => Err(input),
+    }
+}
