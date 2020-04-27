357fd02360376bfe9ad705fe99cddabf12a6812e
swiftaff <swiftaff@gmail.com>
2020-04-27 18:08:30 +0800

    combinators

--- src/lib.rs
+++ src/lib.rs
@@ -40,6 +40,20 @@ fn identifier(input: &str) -> Result<(&str, String), &str> {
     Ok((&input[next_index..], matched))
 }
 
+fn pair<P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Fn(&str) -> Result<(&str, (R1, R2)), &str>
+where
+    P1: Fn(&str) -> Result<(&str, R1), &str>,
+    P2: Fn(&str) -> Result<(&str, R2), &str>,
+{
+    move |input| match parser1(input) {
+        Ok((next_input, result1)) => match parser2(next_input) {
+            Ok((final_input, result2)) => Ok((final_input, (result1, result2))),
+            Err(err) => Err(err),
+        },
+        Err(err) => Err(err),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -68,5 +82,15 @@ mod tests {
             identifier("!not at all an identifier"),
             Err("!not at all an identifier")
         );
+    }
+    #[test]
+    fn test_pair_combinator() {
+        let tag_opener = pair(match_literal("<"), identifier);
+        assert_eq!(
+            tag_opener("<my-first-element/>"),
+            Ok(("/>", ((), "my-first-element".to_string())))
+        );
+        assert_eq!(tag_opener("oops"), Err("oops"));
+        assert_eq!(tag_opener("<!oops"), Err("!oops"));
     }
 }
